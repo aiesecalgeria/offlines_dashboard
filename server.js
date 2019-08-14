7e6536553729834c8b05d20a0c82528dfbf96343
wass <wass@Wass-PC.Home>
2019-08-14 22:16:15 +0100

    Adding Wrapper

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
+const express = require("express"); // get express
+const connectDB = require("./config/db");
 
+const app = express(); // intilize app var
 
-const app = express();
+// connect database
 
+//connectDB();
 
-app.get('/',(req,res) => res.send('API Running'));
+// Init MIddleware
 
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API RUnning")); // end point
+
+// define routes
+
+app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/users", require("./routes/api/users"));
+
+// app.use("/api/profile", require("./routes/api/profile"));
+// app.use("/api/post", require("./routes/api/post"));
+
+// defining the port
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+app.listen(PORT, () => console.log(`Serveur started on port ${PORT}`));
